9fbc54ee776218c863fc2fa3891700e975a4caad
María <mariamsm94@hotmail.com>
2017-03-12 17:22:43 +0100

    he alineado varias cosas y probado el menu... pero el menú no me funciona bien :(

--- Practica 1/JavaScript/formularios.js
+++ Practica 1/JavaScript/formularios.js
@@ -335,3 +335,47 @@ function emailValido(vari){
     return ok;
 }
 
+/* Menú hamburguesa */
+
+$(function(){
+    var pull = $('$pull');
+        menu = $('nav ul');
+        menuHeight = menu.height();
+    $(pull).on('click', function(e)){
+        e.preventDefault();
+        menu.slideToggle();
+    });
+    $(window).resize(function(){
+        var w = $(window).width();
+        if(w > 320 && menu.is(':hidden')){
+            menu.removeAttr('style');
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
