284457cb38cf2aa13a537959fa72b75764ad2470
María <mariamsm94@hotmail.com>
2017-06-02 20:48:12 +0200

    intento de hacer cosas...

--- PRÁCTICA 3/practica3/js/juego.js
+++ PRÁCTICA 3/practica3/js/juego.js
@@ -29,15 +29,7 @@ function f1() {
     if(typeof(Storage) !== "undefined") {
       var usu1=document.getElementById("equipo1").value;
       sessionStorage.setItem("login1",usu1);
-      document.getElementById("form1").innerHTML="<legend>Bienvenido "+sessionStorage.getItem("login1")+"</legend><br><br>";
-
-      if(sessionStorage.getItem("login1") && sessionStorage.getItem("login2")){
-        console.log("mostrar boton Jugar 1");
-
-        document.getElementById("botonJugar").innerHTML="<button onclick='f3();' class='button' value = 'Jugar'>Jugar</button>";
-    
-      }
-
+      document.getElementById("form1").innerHTML="<legend>Bienvenido "+sessionStorage.getItem("login1")+"</legend></br></br>";
     } else {
         document.getElementById("form1").innerHTML = "Sorry...";
     }
@@ -47,15 +39,7 @@ function f2() {
     if(typeof(Storage) !== "undefined") {
       var usu2=document.getElementById("equipo2").value;
       sessionStorage.setItem("login2",usu2);
-      document.getElementById("form2").innerHTML="<legend>Bienvenido "+sessionStorage.getItem("login2")+"</legend><br><br>";
-
-      if(sessionStorage.getItem("login1") && sessionStorage.getItem("login2")){
-        console.log("mostrar boton Jugar 1");
-
-        document.getElementById("botonJugar").innerHTML="<br><button onclick='f3();' class='button' value = 'Jugar'>Jugar</button>";
-    
-      }
-
+      document.getElementById("form2").innerHTML="<legend>Bienvenido "+sessionStorage.getItem("login2")+"</legend></br></br>";
     } else {
         document.getElementById("form2").innerHTML = "Sorry...";
     }
@@ -72,27 +56,6 @@ function f3(){
   }
 }
 
-function actualizaIndex(){
-
-  if(sessionStorage.getItem("login1") && sessionStorage.getItem("login2")){
-    document.getElementById("form1").innerHTML="<legend>Bienvenido "+sessionStorage.getItem("login1")+"</legend><br><br>";
-    document.getElementById("form2").innerHTML="<legend>Bienvenido "+sessionStorage.getItem("login2")+"</legend><br><br>";
-    document.getElementById("botonJugar").innerHTML="<button onclick='f3();' class='button' value = 'Jugar'>Jugar</button>";
-    //window.location="juego.html";
-  }  
-
-  else{
-
-    if(sessionStorage.getItem("login1")){
-      document.getElementById("form1").innerHTML="<legend>Bienvenido "+sessionStorage.getItem("login1")+"</legend>";
-    }
-    else if(sessionStorage.getItem("login2")){
-      document.getElementById("form2").innerHTML="<legend>Bienvenido "+sessionStorage.getItem("login2")+"</legend>";
-    }
-
-  }
-}
-
 function actualizaJuego(){
 
   if(sessionStorage.getItem("login1") && sessionStorage.getItem("login2")){
@@ -103,13 +66,13 @@ function actualizaJuego(){
     document.getElementById("jugador1").innerHTML="<p>Equipo 1: "+sessionStorage.getItem("login1")+"</p>";
     document.getElementById("jugador2").innerHTML="<p>Equipo 2: "+sessionStorage.getItem("login2")+"</p>";
   }else{
-    window.location.href="./index.html";
+    window.location="index.html";
   }
 
 }
 /************************************************************/
-function colorearCasilla(event){
-
+/*function colorearCasilla(event){
+  console.log("entrooo en colorearCasilla ");
   var x = event.clientX;
   var y = event.clientY;
   var dim = c.width/20;
@@ -126,11 +89,11 @@ function colorearCasilla(event){
   
   
   ctx.strokeStyle = "#00a";
-  ctx.strokeRect(parseInt(x/160)*100,parseInt(y/160)*160,160,1180);
+  ctx.strokeRect(parseInt(x/40)*40,parseInt(y/40)*40,40,1180);
   
 
   pintarFichas();
-}
+}*/
 
 function hacerRejilla(){// OKAY
   var c=document.getElementById("canvasJuego");
@@ -139,13 +102,13 @@ function hacerRejilla(){// OKAY
   var dim = c.width/20;//lo que mide un cuadrado de ancho y de largo
   //324 sería para alto
 
-  for(i=2; i<=18; i++){
+  for(i=0; i<=18; i++){
 
     ctx.moveTo(i*dim,0);
     ctx.lineTo(i*dim, c.height);
   }
 
-  for(i=1; i<=9; i++){
+  for(i=0; i<=9; i++){
 
     ctx.moveTo(dim,i*dim);
     ctx.lineTo(c.width-dim, i*dim);
@@ -216,27 +179,97 @@ function hacerRejilla(){// OKAY
   ctx.strokeRect(720-dim,dim*5,dim,dim);
 
 }
+/**************************************************/
+function prepararDnD2(){//CODIGO DEL PROFESOR
+
+  //ZONA DRAG
+  let v = document.querySelectorAll('body>footer>canvas');
+
+  for(let i = 0; i < v.length; i++){
+    v[i].setAttribute('draggable', 'true');
+    v[i].id = 'canvas' + i;
+    v[i].ondragstart = function(e){
+      e.dataTransfer.setData('text/plain', v[i].id);
+    };
+  }
+
+  //ZONA DROP
+  let cv = document.getElementById('canvasJuego');
+
+  cv.ondragover = function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    let x = e.offsetX,
+      y = e.offsetY,
+      dim = cv.width/20,
+      fila= Math.floor(y/dim),
+      columna= Math.floor(x/dim),
+      ctx=cv.getContext('2d');
+
+
+      dibujarCuadricula();
+      ctx.beginPath();
+      ctx.strokeStyle= '#f00';
+      ctx.lineWidth= 3;
+      ctx.strokeRect(columna*dim, fila*dim, dim, dim);
+
+  }
+  cv.ondrop = function(e){
+    e.preventDefault();
+    e.stopPropagation();
+
+    let x = e.offsetX,
+      y = e.offsetY,
+      dim = cv.width/20,
+      fila= Math.floor(y/dim),
+      columna= Math.floor(x/dim),
+      id = e.dataTransfer.getData('text/plain'),
+      ctx = cv.getContext('2d'),
+      img = new Image();
+
+    img.onload = function(){
+      //ctx.drawImage(img, x, y);
+      ctx.drawImage(img,columna*dim,fila*dim,dim, dim);
+      dibujarCuadricula();
+    }
+    img.src = document.getElementById(id).src;
+
+  }
+}
+/*******************************************************/
 
 function pintarFichas(){
       
   var canvas = document.getElementById("canvasJuego");
   var ctx = canvas.getContext("2d");
+  //var dim = c.width/20;
 
     for(var i=0; i<18; i++) {
       for(var j=0; j<9; j++){
         if(tablero[i][j]!='n'){
           if(tablero[i][j]=='r'){
-            ctx.drawImage(ficha_roja,(i)*162,(j)*162);
+            ctx.drawImage(ficha_roja,(i)*40,(j)*40);
             
           }else{
-            ctx.drawImage(ficha_verde,(i)*162,(j)*162);
+            ctx.drawImage(ficha_verde,(i)*40,(j)*40);
           }
         } 
       }
     }
-
 }
 
+/*
+
+function mostrarMatriz(){
+
+  for(var i=0; i<18; i++) {
+    for(var j=0; j<9; j++){
+      console.log(tablero[i][j]);
+      //document.write(tablero[i][j]);    
+    }
+  }
+}
+*/
 
 function clickEnJuego(event){
 
@@ -244,7 +277,7 @@ function clickEnJuego(event){
 
     var x = event.clientX;
     var y = event.clientY;
-    alert("hhoooooolaaaa"+x+y);
+   
     var canvas = document.getElementById("canvasJuego");
     var ctx = canvas.getContext("2d");
     var rect = canvas.getBoundingClientRect();
@@ -256,7 +289,7 @@ function clickEnJuego(event){
       var ctx=c.getContext("2d");
       ctx.clearRect(0,0,c.width,c.height);
       ficha_roja_colocada[ficha]=true;
-      insertaFicha(parseInt(x/162),'r');
+      insertaFicha(parseInt(y/40), parseInt(x/40),'r');
       turnoRojo=false;
 
     }else if(turnoRojo==false && color_ficha=='v'){
@@ -265,7 +298,7 @@ function clickEnJuego(event){
       var ctx=c.getContext("2d");
       ctx.clearRect(0,0,c.width,c.height);
       ficha_verde_colocada[ficha]=true;
-      insertaFicha(parseInt(x/162),'v');
+      insertaFicha(parseInt(y/40), parseInt(x/40),'v');
       turnoRojo=true;
     }
 
@@ -273,9 +306,11 @@ function clickEnJuego(event){
     color_ficha='n';
     ficha=-1;
   }
+  pintarFichas();
+  //mostrarMatriz();
 }
 
-function pintarTurno(){
+function pintarTurno(){// OKEY
   if(turnoRojo==true){
     document.getElementById("jugadorRojo").style.backgroundColor="white";
     document.getElementById("jugadorVerde").style.backgroundColor="#A9E2F3";
@@ -286,16 +321,40 @@ function pintarTurno(){
   }
 }
 
-function insertaFicha(columna,color){
-  var hecho=false;
 
+/*
+
+function insertaFicha(columna,color){
+  var canvas = document.getElementById("canvasJuego");
+  var ctx = canvas.getContext("2d");
+
+  
+  ctx.drawImage(ficha_roja, 0, 0);
+
+}
+
+*/
+
+
+
+
+function insertaFicha(fila, colum, color){
+
+
+console.log("columna: "+colum+"   fila: "+fila);
+
+if(tablero[fila][colum]=='n'){
+  tablero[fila][colum]=color;
+
+}
+/*
     for(var j=18; j>=0; j--){
-        if(tablero[columna][j]=='n' && hecho==false){
-          tablero[columna][j]=color;
+        if(tablero[i][j]=='n' && hecho==false){
+          tablero[i][j]=color;
           console.log("insertado color "+color+" en columna "+columna);
           hecho=true;
         } 
-      }   
+      }   */
 }
 
 function ficha_seleccionada(color,num){
@@ -358,7 +417,7 @@ function limpia_ficha(){
 }
 
 function pinta_ficha_no_colocada(){
-  for(i=0;i<=81;i++){
+  for(i=0;i<=21;i++){
     if(ficha_roja_colocada[i]==false){
     var c=document.getElementById("fichar"+i);
     var ctx=c.getContext("2d");
@@ -366,7 +425,7 @@ function pinta_ficha_no_colocada(){
     ctx.drawImage(ficha_roja,0,0);
     }
   }
-  for(i=0;i<=81;i++){
+  for(i=0;i<=21;i++){
     if(ficha_verde_colocada[i]==false){
     var c=document.getElementById("fichav"+i);
     var ctx=c.getContext("2d");
